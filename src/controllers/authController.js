878e4274d41804b8e366ab636ee97ae6f98de4c7
wh3reis22 <bruceklenam32@gmail.com>
2025-07-29 15:52:00 +0000

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,5 @@
-
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-require('dotenv').config();
-
 exports.register = async (req, res) => {
-  const { fullname, username, password } = req.body;
+  const { fullName, username, email, password } = req.body;
 
   try {
     let user = await User.findOne({ username });
@@ -14,8 +8,9 @@ exports.register = async (req, res) => {
     }
 
     user = new User({
-      fullname,
+      fullName, // match exact casing from frontend or model
       username,
+      email,
       password,
     });
 
@@ -36,7 +31,7 @@ exports.register = async (req, res) => {
       { expiresIn: 3600 },
       (err, token) => {
         if (err) throw err;
-        res.json({ token });
+        res.json({ token, user }); // Also return user if needed
       }
     );
   } catch (err) {
@@ -45,37 +40,3 @@ exports.register = async (req, res) => {
   }
 };
 
-exports.login = async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    let user = await User.findOne({ username });
-    if (!user) {
-      return res.status(400).json({ msg: 'Invalid credentials' });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ msg: 'Invalid credentials' });
-    }
-
-    const payload = {
-      user: {
-        id: user.id,
-      },
-    };
-
-    jwt.sign(
-      payload,
-      process.env.JWT_SECRET,
-      { expiresIn: 3600 },
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token });
-      }
-    );
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-  }
-};
